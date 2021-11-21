4f1211ca496d14a0528700130931cd20cd333ba3
Wpaswan <dablumcapaswan@gmail.com>
2021-11-21 09:17:53 -0800

    Attendance feature added

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -1,2 +1,24 @@
 ﻿
-Console.WriteLine("Welcome to Employee Wage computation programme");
+namespace EmpWage
+{
+    class EmployeeUseCase1
+    {
+        static void Main(string[] args)
+        {
+            int isPresent = 1;
+            Random random = new Random();
+            int randominput = random.Next(0, 2);
+
+            if (randominput == isPresent)
+            {
+                Console.WriteLine("Present");
+            }
+            else
+            {
+                Console.WriteLine("Absent");
+            }
+            Console.ReadLine();
+        }
+    }
+
+}
