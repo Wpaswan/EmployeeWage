8f1ef45261fb92c4e79544d2c1c5e4856dc930a5
Wpaswan <dablumcapaswan@gmail.com>
2021-11-21 09:58:33 -0800

    Employee Wage per hour added

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -4,32 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EmpWage
+namespace EmpWage.UseCase3
 {
-    public class EmployeeUseCase2
+    public class EmployeeUseCase3
     {
         public static void Main(string[] args)
         {
-            int isPresent = 1;
+            int is_Full_Time = 1;
+            int is_Half_Time = 2;
             int empWage = 0;
             int ratePerHrs = 20;
             int empHrs = 0;
 
             Random employee = new Random();
-            int randomInput = employee.Next(0, 2);
+            int randomInput = employee.Next(0, 3);
 
-            if (randomInput == isPresent)
+            if (randomInput == is_Full_Time)
             {
-                Console.WriteLine("Employee is Present");
+                Console.WriteLine("Full Time Employee is Present");
                 empHrs = 8;
             }
-            else 
+            else if(randomInput == is_Half_Time)
+            {
+                Console.WriteLine("Part Time Employee is Present");
+                empHrs = 8 / 2;
+            }
+            else
             {
                 Console.WriteLine("Employee is Absent");
-                empHrs = 0;
             }
             empWage = ratePerHrs * empHrs;
-            Console.WriteLine("Employee wage per day: "+empHrs);
+            Console.WriteLine("Employee wage per day: " + empHrs);
             Console.ReadLine();
         }
     }
